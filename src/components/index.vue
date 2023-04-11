cdb3036329e4a7eafb2949f62281d5743dbeb4d2
diandianjun <1730523754@qq.com>
2023-04-11 20:34:42 +0800

    initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>文档检索管理系统</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
