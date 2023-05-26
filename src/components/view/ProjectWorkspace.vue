161fdb6136cf8a899f3725bdf90e9c95d7f04bd4
Clement Guillaume <1713332640@qq.com>
2023-05-26 17:57:27 +0800

    项目内容展示

--- src/components/view/ProjectWorkspace.vue
+++ src/components/view/ProjectWorkspace.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>    
-    <h1>{{ projectName }}</h1>
-    <h2>{{ projectType }}</h2>
-    <textarea v-model="projectInfo"></textarea>
+  <div>
+    <div class="search-box">
+      <!-- 主搜索框的代码 -->
+    </div>
+
+    <div class="project-section">
+      <h1>{{ projectName }}</h1>
+      <h2>{{ projectType }}</h2>
+      <textarea v-model="projectInfo"></textarea>
+    </div>
   </div>
 </template>
 
@@ -25,5 +31,33 @@ export default {
 </script>
 
 <style scoped>
+.search-box {
+  /* 主搜索框样式 */
+}
 
+.project-section {
+  width: 50%;
+  margin-top: 20px;
+}
+
+.project-section h1 {
+  font-size: 24px;
+  color: #333;
+  margin-bottom: 10px;
+}
+
+.project-section h2 {
+  font-size: 18px;
+  color: #666;
+  margin-bottom: 10px;
+}
+
+.project-section textarea {
+  width: 100%;
+  height: 200px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
 </style>
