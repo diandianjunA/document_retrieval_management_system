4fefec6014160519c15f13aee88070b298555fbf
Clement Guillaume <1713332640@qq.com>
2023-05-26 17:48:42 +0800

    项目内容显示

--- src/components/view/ProjectWorkspace.vue
+++ src/components/view/ProjectWorkspace.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <div>    
+    <h1>{{ projectName }}</h1>
+    <h2>{{ projectType }}</h2>
+    <textarea v-model="projectInfo"></textarea>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ProjectWorkspace"
+  name: "ProjectWorkspace",
+  data() {
+    return {
+      projectName: "",
+      projectType: "",
+      projectInfo: ""
+    };
+  },
+  created() {
+    this.projectName = this.$store.state.projectStore.projectName;
+    this.projectType = this.$store.state.projectStore.projectType;
+    this.projectInfo = this.$store.state.projectStore.projectInfo;
+  }
 }
 </script>
 
